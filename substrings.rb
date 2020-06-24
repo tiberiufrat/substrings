e18f05db36fda8590d764a6375b1f75054a88516
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 11:31:10 +0300

    add testing data

--- substrings.rb
+++ substrings.rb
@@ -5,4 +5,9 @@ def substrings(string, dictionary)
       hash[term] += 1
     end
   end
+  hash
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
